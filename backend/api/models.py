52ed38e5616df1f6e4c8304d0e6f2e2ea77041a6
William Githinji <146653695+Githinji0@users.noreply.github.com>
2025-10-29 12:17:36 +0300

    First Commit

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=40)
+    author = models.CharField(max_length=100)
+    year = models.IntegerField(default=1)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
